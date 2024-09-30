79e4c06dc47919d5dee6552c0d6dcf4d9ab0e873
Thulio-DEV <thuliosergio10@hotmail.com>
2024-09-30 17:50:47 -0300

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello THULIO World!';
+    return 'Hello ok World!';
   }
 }
